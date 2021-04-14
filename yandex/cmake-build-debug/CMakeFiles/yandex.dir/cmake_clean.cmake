27ee5eae397d79fd14d1dba8fa8d396823f0c7e2
PUTIN-POMOGI <78841564+PUTIN-POMOGI@users.noreply.github.com>
2021-04-14 18:48:02 +0300

    Add files via upload

---
+++ yandex/cmake-build-debug/CMakeFiles/yandex.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/yandex.dir/main.cpp.obj"
+  "libyandex.dll.a"
+  "yandex.exe"
+  "yandex.exe.manifest"
+  "yandex.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/yandex.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
